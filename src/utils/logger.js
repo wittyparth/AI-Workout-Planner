c89a30634b75540c3e0e1adbc38dc6e4a86cbf1d
Partha Saradhi <parthasaradhimunakala@gmail.com>
2025-09-29 16:45:59 +0530

    completed day 2

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,18 +1,18 @@
 const { NODE_ENV } = require("../config/environment")
 
 class Logger {
-    info(message,meta={}){
-        console.log(`[INFO] ${new Date().toISOString()} - ${message}`,meta)
+    info(message, meta = {}) {
+        console.log(`[INFO] ${new Date().toISOString()} - ${message}`, meta)
     }
-    warn(message,meta={}){
-        console.warn(`[WARN] ${new Date().toISOString()} - ${message}`,meta)
+    warn(message, meta = {}) {
+        console.warn(`[WARN] ${new Date().toISOString()} - ${message}`, meta)
     }
-    error(message,meta={}){
-        console.error(`[ERROR] ${new Date().toISOString()} - ${message}`,meta)
+    error(message, meta = {}) {
+        console.error(`[ERROR] ${new Date().toISOString()} - ${message}`, meta)
     }
-    debug(message,meta){
-        if(NODE_ENV==="development"){
-            console.log(`[DEBUG] ${new Date().toISOString()} - ${message}`,meta)
+    debug(message, meta = {}) {
+        if (NODE_ENV === "development") {
+            console.log(`[DEBUG] ${new Date().toISOString()} - ${message}`, meta)
         }
     }
 }
